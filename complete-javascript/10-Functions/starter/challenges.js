51ad0496b415bc02c78c7e15bf580bdf612ad632
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-09-27 14:26:32 +0200

    add bind() method challenges

--- complete-javascript/10-Functions/starter/challenges.js
+++ complete-javascript/10-Functions/starter/challenges.js
@@ -7,10 +7,68 @@
 const poll = {
     question: 'What is your favourite programming language?',
     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-    // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+
+    registerNewAnswer() {
+        // Get number
+        const answer = Number(
+            prompt(
+                `${this.question} \n${this.options.join(
+                    '\n'
+                )}\n(Write option number)`
+            )
+        );
+
+        // Register answer
+        // use short circuiting instead of if statement
+        typeof answer === 'number' &&
+            answer <= this.answers.length &&
+            this.answers[answer]++;
+    },
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            console.log(`Poll results are: ${this.answers.join(', ')}`);
+        }
+    },
 };
 
-console.log(poll);
+// Use bind() to clarify the target of the this keyword (poll)
+document
+    .querySelector('.poll')
+    .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// call a new array as 'this'
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+
+///////////////////////// Challenge 1
+
+function greet(name) {
+    console.log(name); // Chris
+    console.log(this); // {name: 'John'}
+    console.log(`Hello, ${name}! My name is ${this.name}.`);
+}
+
+const person = {
+    name: 'John',
+};
+
+// Bind person object as 'this' value
+// Prefill name parameter as 'Chris'
+const greetJohn = greet.bind(person, 'Chris');
+
+greetJohn(); // Hello, Chris! My name is John
+
+///////////////////////// Challenge 2
+
+function multiply(x, y) {
+    return x * y;
+}
+
+// Create a new function 'double' using bind() that always doubles the given number.
+// Call 'double' to double a specific number.
 
 ////////////////////////////////////////////////////////////
 // Coding challenge #2
