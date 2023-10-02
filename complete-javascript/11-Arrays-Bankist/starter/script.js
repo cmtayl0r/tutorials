3c82341171c4cc0ec9c04700ac8468ef667e9b52
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-10-02 17:50:25 +0200

    add map method experiments

--- complete-javascript/11-Arrays-Bankist/starter/script.js
+++ complete-javascript/11-Arrays-Bankist/starter/script.js
@@ -92,13 +92,13 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const currencies = new Map([
-//     ['USD', 'United States dollar'],
-//     ['EUR', 'Euro'],
-//     ['GBP', 'Pound sterling'],
-// ]);
+const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -148,4 +148,19 @@ numbers.forEach(function (number) {
 
 ///////////////////// Lecture 149 Data transformations: map, filter, reduce
 
+// 150 - map method
 
+/*
+const euroToUsd = 1.1;
+
+const movementsUSD = movements.map(value => (value * euroToUsd).toFixed(2));
+console.log(movementsUSD);
+
+const moveList = movements.map(function (mov, i) {
+    return `Movement ${
+        i + 1
+    }: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`;
+});
+
+console.log(moveList);
+*/
