df4b0788da1f5cb85c8fa2579459b678e212b686
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-10-17 20:05:39 +0200

    add modal toggle function

--- complete-javascript/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript/13-Advanced-DOM-Bankist/starter/script.js
@@ -1,31 +1,35 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// -----------------------------------------------------------------------------
+// ⚙️ FN: MODAL WINDOW
+// -----------------------------------------------------------------------------
 
+// DOM elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+const closeModal = function (e) {
+    // Stop # on a links moving page to top
+    e.preventDefault();
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+    btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
