5a9d743e3e80df567662572e3cfee4a2d00f0dbb
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-10-18 11:08:56 +0200

    add modal forEach fix

--- complete-javascript/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript/13-Advanced-DOM-Bankist/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // -----------------------------------------------------------------------------
-// ⚙️ FN: MODAL WINDOW
+// ⚙️ MODAL WINDOW
 // -----------------------------------------------------------------------------
 
 // DOM elements
@@ -10,24 +10,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+// Open modal function
+const openModal = function (e) {
+    // Stop # on a links moving page to top
+    e.preventDefault();
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
 
-const closeModal = function (e) {
-    // Stop # on a links moving page to top
-    e.preventDefault();
+// Close modal function
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
+// Add event listener for each of the buttons that trigger the modal (two)
+// Provide callback function to forEach
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
+// Close modal via button
 btnCloseModal.addEventListener('click', closeModal);
+// Close modal via overlay
 overlay.addEventListener('click', closeModal);
-
+// Close modal via esc key
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
