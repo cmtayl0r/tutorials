c3822d8a3474449bc07c6e799dff8567edba066e
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-10-24 16:30:55 +0200

    add Classes experiments

--- complete-javascript/14-OOP/starter/script.js
+++ complete-javascript/14-OOP/starter/script.js
@@ -166,6 +166,156 @@ audi.speedUS = 50;
 console.log(audi);
 */
 
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} 👋 and I study ${this.course}.`);
+};
+
+const mike = new Student('Mike', '2020', 'Computer Science');
+
 // -----------------------------------------------------------------------------
 // CHALLENGE 3
 // -----------------------------------------------------------------------------
+/*
+// Parent "class"
+const Car = function (make, speed) {
+    this.speed = speed;
+    this.make = make;
+};
+
+// Parent methods
+Car.prototype.accelerate = function () {
+    console.log(`${this.make} going ${(this.speed += 10)} km/h`);
+};
+
+Car.prototype.brake = function () {
+    console.log(`${this.make} going ${(this.speed -= 5)} km/h`);
+};
+
+// Child "class"
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+// Link prototypes
+EV.prototype = Object.create(Car.prototype);
+
+// Child methods
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+};
+// accelerate method used as its the first in the prototype chain for the EV object
+EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(
+        `${this.make} is going at ${this.speed} km/h with a charge of ${this.charge}% 🚗`
+    );
+};
+
+// EV objects
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate(); // Tesla is going at 140 with a charge of 22% 🚗
+tesla.chargeBattery(90);
+tesla.accelerate(); // Tesla is going at 160 with a charge of 89% 🚗
+*/
+
+/*
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+}
+
+class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        // super = constructor function of parent class;
+        // Needs to be defined first
+        super(fullName, birthYear);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(
+            `My name is ${this.firstName} 👋 and I study ${this.course}.`
+        );
+    }
+}
+
+const chris = new StudentCl('Chris Taylor', 1980, 'Computer Science');
+*/
+
+// LESSON 222 - ANOTHER CLASS EXAMPLE
+
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+
+class Account {
+    // Public fields
+    locale = navigator.language;
+    // Private fields
+    #movements = [];
+    #pin;
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.#pin = pin;
+    }
+
+    // Public methods (Public interface of object)
+    getMovements() {
+        return this.#movements;
+    }
+
+    deposit(value) {
+        this.#movements.push(value);
+        return this; // Makes method chainable
+    }
+    withdraw(value) {
+        this.deposit(-value);
+        return this;
+    }
+
+    // Private method used by another internal method only
+    #approveLoan(value) {
+        return true;
+    }
+    requestLoan(value) {
+        if (this.#approveLoan(value)) {
+            this.deposit(value);
+            console.log('💰 Loan Approved!');
+            return this;
+        }
+    }
+}
+
+const acc1 = new Account('Chris', 'EUR', 1111);
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(2000);
+console.log(acc1);
+console.log(acc1.getMovements());
