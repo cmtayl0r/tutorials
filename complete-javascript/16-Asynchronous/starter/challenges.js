30f51fc397f1ba95eb306c87b24359b7ae048479
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-11-22 23:18:55 +0100

    add challenge 1 solution

--- complete-javascript/16-Asynchronous/starter/challenges.js
+++ complete-javascript/16-Asynchronous/starter/challenges.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const whereAmI = function (lat, lng) {
+    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            console.log(`You are in ${data.city}, ${data.country}`);
+            return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            return renderCountry(data[0]);
+        })
+        .catch(err => {
+            renderError(`Summat went wrong bruv ${err}`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        });
+};
+
+whereAmI('-33.933', '18.474');
