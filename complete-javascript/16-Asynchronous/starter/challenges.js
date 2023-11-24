332cb37ace1983e861156d7de0931a0306f2db43
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-11-24 09:53:25 +0100

    add challenge 1 solution

--- complete-javascript/16-Asynchronous/starter/challenges.js
+++ complete-javascript/16-Asynchronous/starter/challenges.js
@@ -1,24 +1,41 @@
 'use strict';
 
+// CHALLENGE 1
+
 const whereAmI = function (lat, lng) {
+    // Fetch data from Geocode API
     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-        .then(response => response.json())
+        // Get and convert the response to JSON and return it as a new promise
+        .then(response => {
+            // Reject promise if geocode data not found
+            if (!response.ok)
+                throw new Error(`Problem with geocoding ${response.status}`);
+            return response.json();
+        })
+        // Do something with the response object
         .then(data => {
-            // console.log(data);
             console.log(`You are in ${data.city}, ${data.country}`);
+            // fetch data from new API using data from original API
             return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
         })
-        .then(response => response.json())
+        // Convert the response to JSON and return it as a new promise
+        .then(response => {
+            // Reject promise if country not found
+            if (!response.ok)
+                throw new Error(`Country not found ${response.status}`);
+            return response.json();
+        })
+        // Do something with the response object
         .then(data => {
-            // console.log(data);
+            // Run helper function to render country
             return renderCountry(data[0]);
         })
         .catch(err => {
-            renderError(`Summat went wrong bruv ${err}`);
+            renderError(`Summat went wrong bruv ${err.message}`);
         })
         .finally(() => {
             countriesContainer.style.opacity = 1;
         });
 };
 
-whereAmI('-33.933', '18.474');
+whereAmI('52.508', '13.381');
