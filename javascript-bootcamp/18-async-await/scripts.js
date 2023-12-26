0701f28f61de41b13780c84e1a67d55c3cf60e20
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-12-26 12:52:22 +0100

    add section 18 starter files

--- javascript-bootcamp/18-async-await/scripts.js
+++ javascript-bootcamp/18-async-await/scripts.js
@@ -2,6 +2,42 @@
 
 /*
 --------------------------------------------------------------------------------
-181 - Fetch
+181 - xxx
 --------------------------------------------------------------------------------
 */
+
+/*
+async function greet() {
+  return "Hello!";
+}
+greet().then(val => {
+  console.log("PROMISE RESOLVED WITH:", val);
+});
+// PROMISE RESOLVED WITH: Hello!
+
+async function add(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw "X and Y must be numbers!";
+  }
+  return x + y;
+}
+add(2, 3).then(val => console.log(val));
+// 5
+*/
+
+// PROMISE BASED
+const getPlanets = function () {
+  return fetch("https://swapi.dev/api/planets/");
+};
+
+getPlanets()
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+// ASYNC AWAIT
+async function getPlanetz() {
+  const res = await fetch("https://swapi.dev/api/planets/");
+  return res.json();
+}
+
+getPlanetz().then(res => console.log(res));
