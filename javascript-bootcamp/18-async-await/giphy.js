15a7b9867ef0afdc542e21fe6e10ca7a589c5d04
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2023-12-30 00:15:35 +0100

    add odin async await starter files

--- javascript-bootcamp/18-async-await/giphy.js
+++ javascript-bootcamp/18-async-await/giphy.js
@@ -4,3 +4,6 @@
 GIPHY - https://www.theodinproject.com/lessons/node-path-javascript-async-and-await
 --------------------------------------------------------------------------------
 */
+
+// DOM elements
+const img = document.querySelector("#gif");
