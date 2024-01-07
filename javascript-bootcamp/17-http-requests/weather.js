daf17b48195d0f1429971e334d4571a3ce7b69d0
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-01-07 18:25:11 +0100

    add comments on next steps

--- javascript-bootcamp/17-http-requests/weather.js
+++ javascript-bootcamp/17-http-requests/weather.js
@@ -1,47 +1,18 @@
 'use strict';
 
-// https://api.weatherapi.com/v1/current.json?key=3d4405718ac2405eb42205350231312&q=london
-/*
-const fetchData = function (city) {
-    // Fetch returns a promise
-    // No need to wrap fetch in a "new Promise" (promise wrapping)
-    return fetch(
-        `https://api.weatherapi.com/v1/current.json?key=3d4405718ac2405eb42205350231312&q=${city}`
-    ).then(response => {
-        if (response.ok) {
-            // Return parsed response object
-            return response.json();
-        } else {
-            // Response not ok, throw error
-            // throw to external handling (catch) when function called
-            throw new Error('Data response was not ok 🥺');
-        }
-    });
-};
-
-fetchData('berlin').then(data => {
-    console.log(data);
-
-    const testData = document.querySelector('.test-data');
-    testData.textContent = data.location.name;
-
-    const icon = document.createElement('img');
-    document.body.appendChild(icon);
-    icon.setAttribute('src', data.current.condition.icon);
-});
-*/
-
-// https://openweathermap.org/current
-// https://openweathermap.org/forecast5
-
+// FEATURES
+// Show realtime and 5 day forecast data
+// Search input to search for location
 // Switch between C and F units
 // Have an autocomplete search to select location
+// Background image change dependent on weather (use unsplash etc)
+// Mobile version tab switch between realtime and forecast
 
 // DOM ELEMENTS
 const realSection = document.querySelector('#realtime');
 const foreSection = document.querySelector('#forecast');
 
-// Variable to set metric unit
+// Variable to set measure unit default
 let units = 'metric';
 
 async function fetchPlace(location) {
@@ -84,5 +55,12 @@ async function fetchPlace(location) {
 // Function for realtime data
 
 // Function for forecast data
+
+/*
+testData.textContent = data.location.name;
+const icon = document.createElement('img');
+document.body.appendChild(icon);
+icon.setAttribute('src', data.current.condition.icon);
+*/
 
 fetchPlace('Berlin');
