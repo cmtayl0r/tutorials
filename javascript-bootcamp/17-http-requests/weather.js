be9c1d8a38aa88bf74f7fed5670d02d862c75825
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-01-07 18:04:07 +0100

    add weather app async function for API call

--- javascript-bootcamp/17-http-requests/weather.js
+++ javascript-bootcamp/17-http-requests/weather.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // https://api.weatherapi.com/v1/current.json?key=3d4405718ac2405eb42205350231312&q=london
-
+/*
 const fetchData = function (city) {
     // Fetch returns a promise
     // No need to wrap fetch in a "new Promise" (promise wrapping)
@@ -29,3 +29,40 @@ fetchData('berlin').then(data => {
     document.body.appendChild(icon);
     icon.setAttribute('src', data.current.condition.icon);
 });
+*/
+
+// https://openweathermap.org/current
+// https://openweathermap.org/forecast5
+
+// DOM ELEMENTS
+const realSection = document.querySelector('#realtime');
+const foreSection = document.querySelector('#forecast');
+
+// Variable to set metric unit
+let units = 'metric';
+
+async function fetchPlace(location) {
+    try {
+        const rtPromise = fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=53901fa0797c3c9403358b2d02c1f9c8&units=&${units}`
+        );
+        const fcPromise = fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=53901fa0797c3c9403358b2d02c1f9c8&units=&${units}`
+        );
+        const responses = await Promise.all([rtPromise, fcPromise]);
+        let [realtime, forecast] = await Promise.all(
+            responses.map(res => res.json())
+        );
+        console.log(realtime);
+        console.log(forecast);
+    } catch (error) {
+        console.log('There iz an Ezzor:', error.message);
+        realSection.insertAdjacentHTML('beforeend', `<p>${error.message}</p>`);
+    }
+}
+
+// Function for realtime data
+
+// Function for forecast data
+
+fetchPlace('Berlin');
--- javascript-bootcamp/18-async-await/giphy.js
+++ javascript-bootcamp/18-async-await/giphy.js
@@ -23,12 +23,13 @@ const img = document.querySelector('#gif');
 async function getDogs(searchQuery) {
     try {
         let response = await fetch(
-            `https://api.giphy.com/v1/gifs/translate?api_key=Exa5gXbGLGErrkoWHv0IvkXzjE12ktDZ&s=${searchQuery}`
+            `https://api.giphy.com/v1/gifs/translate?api_key=Exa5gXbGLGErrkoWHv0IvkXzjE12ktDZ&s=${searchQuery}, {mode: 'cors'}`
         );
-        let giphyData = await response.json();
-        img.src = giphyData.data.images.original.url;
+        let fetchData = await response.json();
+        img.src = fetchData.data.images.original.url;
     } catch (err) {
         console.error(err);
+        // img.src = "empty state element"
     }
 }
 
