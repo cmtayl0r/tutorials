98f8f4abec9247c72a509c4c9418a9d290a47e46
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-01-13 13:11:14 +0100

    add examples explaining js modules

---
+++ complete-javascript/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,7 @@
+// Importing module
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addToCart('Bread', 5);
+// console.log(totalPrice, totalQuantity);
