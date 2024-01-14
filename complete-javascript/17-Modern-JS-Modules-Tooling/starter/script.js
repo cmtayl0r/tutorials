52a5f7b6ab27d473fd39faf018685073c0af7cd4
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-01-14 12:50:02 +0100

    add parcel and bumdling test files

--- complete-javascript/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,7 +1,17 @@
 // Importing module
 // import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
-
 import * as ShoppingCart from './shoppingCart.js';
+
+// loadash-es package file
+import { cloneDeep } from 'lodash-es';
+
+const state = {
+    cart: [
+        { product: 'Bread', quantity: 5 },
+        { product: 'Pizza', quantity: 3 },
+    ],
+    user: { loggedIn: true },
+};
 
 ShoppingCart.addToCart('Bread', 5);
 // console.log(totalPrice, totalQuantity);
