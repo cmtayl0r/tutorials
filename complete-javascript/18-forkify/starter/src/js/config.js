df42a82b12cf00dcaea7b8b1f0988a00eac5b466
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-02-21 16:48:06 +0100

    add congig.js file

---
+++ complete-javascript/18-forkify/starter/src/js/config.js
@@ -0,0 +1,11 @@
+// Contains all the variables that should be reused across the project
+// Important data about the application
+// Goal is to allow easy configuration of data in this file
+
+////////////////////////////////////////////////////////////////////////////////
+// VARIABLES
+////////////////////////////////////////////////////////////////////////////////
+
+// API URL
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+// API KEY
