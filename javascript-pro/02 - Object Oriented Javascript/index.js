4c00e020602e36d8d6bf36e25525082f90b769ef
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-03 19:50:35 +0200

    add Section 2 Lesson 11 solution

--- javascript-pro/02 - Object Oriented Javascript/index.js
+++ javascript-pro/02 - Object Oriented Javascript/index.js
@@ -3,28 +3,37 @@
 // -----------------------------------------------------------------------------
 
 class BankAccount {
-  constructor(balance = 0, accHolder, accNumber) {
+  constructor(accNumber, accHolder, balance = 0) {
     this.balance = balance;
     this.accHolder = accHolder;
     this.accNumber = accNumber;
   }
   deposit(amt) {
-    if (!this.balance >= 0) return parseInt((this.balance += amt));
+    if (amt > 0) {
+      this.balance += amt;
+      console.log(`Deposited €${amt}. New Balance: €${parseInt(this.balance)}`);
+    }
+    console.error(`Can't deposit a negative amount`);
   }
   withdraw(amt) {
-    if (!this.balance >= 0 || amt > this.balance) {
-      console.error("Not enough money");
+    if (this.balance <= 0 || amt > this.balance) {
+      console.error("Not enough money 🥺");
     } else {
-      return parseInt((this.balance -= amt));
+      this.balance -= amt;
+      console.log(`Withdrawn €${amt}. New Balance: €${parseInt(this.balance)}`);
     }
-  }
-  showData() {
-    console.log(this.balance);
   }
 }
 
-const newAccount = new BankAccount();
+const newAccount = new BankAccount("Chris Taylor", "12345678");
+
+console.log(newAccount);
 
 newAccount.deposit(50);
-newAccount.withdraw(60);
-newAccount.showData();
+console.log(newAccount.balance);
+newAccount.withdraw(30);
+newAccount.withdraw(30);
+console.log(newAccount.balance);
+
+// newAccount.withdraw(60);
+// newAccount.showData();
