3b08921c42ad59267aedf99bd4f9f94a4e8275f6
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-06 18:28:27 +0200

    add Section 3 class code

--- javascript-pro/03 - OOP New Features/index.js
+++ javascript-pro/03 - OOP New Features/index.js
@@ -14,12 +14,63 @@ class User {
 
 const you = new User('Chris', 'Taylor');
 
-// you.fullName('Ted Lasso');
+// console.log(you.fName);
+// console.log(you.lName);
 
-console.log(you.fName);
-console.log(you.lName);
+// you.setFullName = 'Ted Lasso';
 
-you.setFullName = 'Ted Lasso';
+// console.log(you.fName);
+// console.log(you.lName);
 
-console.log(you.fName);
-console.log(you.lName);
+// -----------------------------------------------------------------------------
+// CODING EXERCISE 3
+// -----------------------------------------------------------------------------
+
+class UserProfile {
+    constructor(username, email, birthDate) {
+        if (username === '' || typeof username !== 'string') {
+            throw new Error('Invalid username.');
+        }
+        if (!email.includes('@')) {
+            throw new Error('Invalid email.');
+        }
+        if (!Date.parse(birthDate)) {
+            throw new Error('Invalid birthdate.');
+        }
+        this._username = username;
+        this._email = email;
+        this._birthDate = birthDate;
+    }
+
+    // GETTER METHODS
+    get userName() {
+        return this._username;
+    }
+    get email() {
+        return this._email;
+    }
+    get birthDate() {
+        return this._birthDate;
+    }
+
+    // SETTER METHODS
+    set username(username) {
+        if (username === '' || typeof username !== 'string') {
+            throw new Error('Invalid username.');
+        }
+        this._username = username;
+    }
+    set email(email) {
+        if (!email.includes('@')) {
+            throw new Error('Invalid email.');
+        }
+        this._email = email;
+    }
+    set birthDate(date) {
+        // 1990-01-01
+        if (!Date.parse(date)) {
+            throw new Error('Invalid birthdate.');
+        }
+        this._birthDate = date;
+    }
+}
