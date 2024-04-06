b806c08246ac4fa7f84dbc481f60e658ce10f2ac
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-06 20:27:35 +0200

    add Section 4 snippets

--- javascript-pro/04 - This keyword/index.js
+++ javascript-pro/04 - This keyword/index.js
@@ -1,3 +1,30 @@
 // -----------------------------------------------------------------------------
-// CODING EXERCISE x
+// Binding arguments
 // -----------------------------------------------------------------------------
+
+function applyVAT(taxRate, price) {
+    return price + price * taxRate;
+}
+
+// "null" means that the "this" keyword will be ignored
+const applyUKVAT = applyVAT.bind(null, 0.2);
+const applyGermanVAT = applyVAT.bind(null, 0.19);
+applyUKVAT(100); // 120
+applyGermanVAT(100); // 119
+
+// -----------------------------------------------------------------------------
+// Bind method and event listeners
+// -----------------------------------------------------------------------------
+
+const chris = {
+    name: 'Chris',
+    city: 'Berlin',
+    sing() {
+        console.log(`${this.name} is singing`);
+    },
+};
+
+const button = document.querySelector('#clickMe');
+// create a new function that is bound to the "chris" object
+// Set this to be the chris object
+button.addEventListener('click', chris.sing.bind(chris));
