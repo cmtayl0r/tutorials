a879a3adb430be46fdc645928ca8ce02309c6011
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-14 21:03:01 +0200

    add debounce function code

--- javascript-pro/10 - Timers/index.js
+++ javascript-pro/10 - Timers/index.js
@@ -19,9 +19,94 @@ showNotification('This is a notification message', 4000);
 // SETINTERVAL
 // -----------------------------------------------------------------------------
 
+// Random colors
+const colorBlocks = document.querySelectorAll('.clr-blk');
+
+function randomRGB() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+// function to change the color of the blocks
+function randomColors() {
+    // loop through the color blocks and change the color
+    colorBlocks.forEach(div => {
+        div.style.backgroundColor = randomRGB();
+    });
+}
+// call the function every 1.5 seconds
+// setInterval(randomColors, 1000);
+
+// countdown
+const timerText = document.querySelector('.timer');
+
+function startCountdown(duration) {
+    let secondsRemaining = duration;
+    const timer = setInterval(function () {
+        console.log(`⏲️ ${secondsRemaining} seconds remain`);
+        timerText.innerText = `⏲️ ${secondsRemaining}`;
+        secondsRemaining--;
+
+        if (secondsRemaining === -1) {
+            console.log('Timer finished!');
+            timerText.innerText = `🏁 Timer finished!`;
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+// startCountdown(10);
+
 // -----------------------------------------------------------------------------
 // DEBOUNCE
 // -----------------------------------------------------------------------------
+
+const searchInput = document.querySelector('#search');
+
+// Debounce function
+// This function will return a new function that will be called ...
+// every time an event is triggered
+// parameters are the callback function and the delay
+function debounce(callback, delay = 1000) {
+    // variable to store the timeoutID
+    let timeoutID;
+
+    // return a new function that will be called every time the event is triggered
+    // spread operator to get all the arguments from the event
+    return function (...args) {
+        // save the context of the function
+        const context = this;
+
+        // if event is triggered again, the timeout will be cleared
+        clearTimeout(timeoutID);
+
+        // if event is not triggered again, the timeout will be set
+        timeoutID = setTimeout(() => {
+            // when timeout is finished ...
+            // apply the callback function with the arguments and context
+            callback.apply(context, args);
+        }, delay);
+    };
+}
+
+// Function to debounce on input entry
+function fakeQueryAPI(searchTerm, otherArg) {
+    console.log(`SEARCHING THE API for ${searchTerm}`);
+    console.log('MAKING AN API REQUEST');
+    console.log(`ANOTHER ARGUMENT: ${otherArg}`);
+}
+
+// const debouncedQueryAPI = debounce(fakeQueryAPI, 1000);
+
+searchInput.addEventListener(
+    'input',
+    debounce(() => {
+        // 'this' refers to the searchInput
+        fakeQueryAPI(this.value, '2nd Argument');
+    }, 1000)
+);
 
 // -----------------------------------------------------------------------------
 // THROTTLE
