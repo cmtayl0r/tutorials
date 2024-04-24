b4d3810c7ba6353c882812123776131803622478
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-24 20:10:15 +0200

    add getCurrentPosition snippet

--- javascript-pro/14 - Geolocation/index.js
+++ javascript-pro/14 - Geolocation/index.js
@@ -1,3 +1,22 @@
 // -----------------------------------------------------------------------------
-// GEO LOCATION
+// GEOLOCATION
 // -----------------------------------------------------------------------------
+
+const locationDisplay = document.querySelector('#locationDisplay');
+
+document.querySelector('#getLocation').addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayGeoData, displayError);
+    }
+});
+
+const displayGeoData = position => {
+    console.log(position);
+    const { latitude, longitude } = position.coords;
+    locationDisplay.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+};
+
+const displayError = error => {
+    locationDisplay.textContent = error.message || 'An unknown error occurred';
+    console.log(error);
+};
