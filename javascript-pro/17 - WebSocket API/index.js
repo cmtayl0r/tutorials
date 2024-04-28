0ed2fe81b0e9400e3e2cce52fda9d407256b3d5a
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-04-28 15:00:34 +0200

    add observer pattern snippets

--- javascript-pro/17 - WebSocket API/index.js
+++ javascript-pro/17 - WebSocket API/index.js
@@ -40,3 +40,62 @@ async function showNotification() {
     }
 }
 btnNotification.addEventListener('click', showNotification);
+
+// -----------------------------------------------------------------------------
+// xxx
+// -----------------------------------------------------------------------------
+
+// Class to define a subject, specifically a blog that manages subscribers
+class Blog {
+    constructor() {
+        // Initialise an Array to hold subscribers
+        this.subscribers = [];
+    }
+
+    subscribe(subscriber) {
+        // Adds the subscriber to the subscribers array
+        this.subscribers.push(subscriber);
+    }
+
+    unsubscribe(subscriber) {
+        // Removes the subscriber from the subscribers array ...
+        // by filtering out the subscriber that is passed in, keeping only those not equal to the given subscriber
+        this.subscribers = this.subscribers.filter(subs => subs !== subscriber);
+    }
+
+    publish(post) {
+        // Loops through all subscribers ...
+        // and calls their notify method, passing in the post object
+        this.subscribers.forEach(subscriber => subscriber.notify(post));
+    }
+}
+
+// Class to define a subscriber
+class Subscriber {
+    constructor(name) {
+        this.name = name;
+    }
+    // method to notify the subscriber of a new post
+    notify(post) {
+        console.log(`${this.name}, a new post published: ${post.title}`);
+    }
+}
+
+// Create new subscriber instances
+const chris = new Subscriber('Chris');
+const john = new Subscriber('John');
+const lily = new Subscriber('Lily');
+
+// Create a new blog instance
+const puppyBlog = new Blog();
+
+// Adding subscribers to the new blog
+puppyBlog.subscribe(chris);
+puppyBlog.subscribe(lily);
+
+// Publishing a new post to the blog; subscribers Chris and Lily are notified
+puppyBlog.publish({
+    title: 'Poopy Pops 🐶 is too tired to walk in the Sun ☀️',
+});
+// Output: Chris, a new post published: Poopy Pops 🐶 is too tired to walk in the Sun ☀️
+// Output: Lily, a new post published: Poopy Pops 🐶 is too tired to walk in the Sun ☀ ️
